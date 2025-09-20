869a3fd580e38df91dc381d8c5340127350794af
Davi Lima <daviamorimdelima3@gmail.com>
2025-09-20 18:37:25 -0300

    finalização layout, agora é so ajeitar e melhorar

--- js/idea.js
+++ js/idea.js
@@ -2,17 +2,27 @@ document.addEventListener('DOMContentLoaded', () => {
     const cardContainer = document.querySelector('.idea-card-container');
     const frontCard = document.querySelector('.idea-card-front');
     const closeButton = document.querySelector('.close-idea-card');
+    const professionsImage = document.querySelector('.professions-image-wrapper img');
 
     // Função para abrir o card
     function openCard() {
         cardContainer.classList.add('is-flipped');
+        document.body.classList.add('light-theme');
+        if (professionsImage) {
+            professionsImage.src = 'img/sessions/lista_session.webp'; // Caminho para a imagem colorida
+        }
     }
+    
 
     // Função para fechar o card
     function closeCard(event) {
         // Impede que o clique no "X" acione a abertura novamente
         event.stopPropagation(); 
         cardContainer.classList.remove('is-flipped');
+        document.body.classList.remove('light-theme');
+        if (professionsImage) {
+            professionsImage.src = 'img/sessions/lista_session-cinza.png'; // Caminho para a imagem cinza
+        }
     }
 
     // Adiciona os eventos de clique
